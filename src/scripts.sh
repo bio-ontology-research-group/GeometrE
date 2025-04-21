9069affc4af5e035b43ee30ddd3f9da982e25261
Fernando Zhapa-Camacho <ferzcam@gmail.com>
2025-04-21 14:08:46 +0300

    :art: added scripts with best hyperparams

---
+++ src/scripts.sh
@@ -0,0 +1,8 @@
+# WN18RR
+python main.py --do_train --do_test --alpha=0.5 --batch_size=1024 --data_path=data/WN18RR-QA --gamma=20 --hidden_dim=400 --learning_rate=0.001 --negative_sample_size=64 --transitive=no --with_answer_embedding
+
+# NELL
+python main.py --do_train --do_test --alpha=0.2 --batch_size=1024 --data_path=data/NELL-betae --gamma=10 --hidden_dim=400 --learning_rate=0.0005 --negative_sample_size=64 --transitive=no
+
+# FB15k-237
+python main.py --do_train --do_test --alpha=0.2 --batch_size=1024 --data_path=data/FB15k-237-betae --gamma=20 --hidden_dim=400 --learning_rate=0.0005 --negative_sample_size=64 --transitive=no
